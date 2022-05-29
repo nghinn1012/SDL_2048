b8a5e5fbac88e5f4bf5eace4fe1f765c5fa5b0ba
nghinn1012 <100118053+nghinn1012@users.noreply.github.com>
2022-05-29 13:34:39 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,15 @@ Game* ptrGame = nullptr;
 Application* app = nullptr;
 
 int main(int argv, char* argo[]){
+	 SDL_Init(SDL_INIT_AUDIO);
+         Mix_Music* music = NULL;
+         Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT,2,512);
+         music = Mix_LoadMUS("monkey.mp3");
+         if (music == NULL)
+            {
+	     printf("%s", Mix_GetError());
+            }
+    Mix_PlayMusic(music,-1);
 	srand(static_cast<unsigned int>(time(NULL)));
 
 	//tạo cửa sổ chơi
